8109b00b49ffcc990cd08258592759badd722e7c
FajarAdityaNugraha <fajaradityanugraha01@gmail.com>
2023-10-05 11:42:18 +0700

    Repository PemilihanPercobaan1No10

---
+++ PemilihanPercobaan1No10.java
@@ -0,0 +1,17 @@
+/**
+ * PemilihanPercobaan1No10
+ */
+import java.util.Scanner;
+public class PemilihanPercobaan1No10 {
+
+    public static void main(String[] args) {
+        Scanner input10 = new Scanner(System.in);
+        System.out.print("Masukan angka : ");
+        int angka = input10.nextInt();
+        if (angka % 2 == 0)
+        System.out.println("Angka "+angka+" bilangan genap");
+        else
+        System.out.println("Angka "+angka+" bilangan ganjil");
+
+    }
+}
